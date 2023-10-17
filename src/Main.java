629d0c270f65d5a44dce8bb98e0fbd2ccc723342
mbaghish <hamedba@icloud.com>
2023-10-17 17:00:39 +0200

    initial commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println("so ok now i wanna make something!");
+        System.out.println("let's go for it!");
     }
 }
